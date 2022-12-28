6f4459004f365dd2936172a421bcee1cf2d05107
cassiozareck <cassiomilczareck@gmail.com>
2022-12-28 14:39:59 -0300

    login now returns json user account to post requests

--- hello.js
+++ hello.js
@@ -43,6 +43,14 @@ function getObj(file) {
     return []
 }
 
+// Add a JSON object to given file without rewriting
+// over it
+function addToJSONFile(obj, file) {
+    let data = getObj(file)
+    data.push(obj)
+    writeToFile(JSON.stringify(data), file)
+}
+
 // Clean a file
 function clean(file) {
     writeToFile('', file)
@@ -50,8 +58,9 @@ function clean(file) {
 
 /* -=============== ACCOUNT AND LOGIN MANAGEMENT */
 class User {
-    constructor(name) {
+    constructor(name, email) {
         this.name = name
+        this.email = email
     }
 }
 
@@ -63,27 +72,52 @@ class User {
 function login(email, passw) {
     let accounts = getObj(FILE_ACCOUNTS)
     for (let i = 0; i < accounts.length; i++) {
-        if (accounts[i].user == email && 
+        if (accounts[i].email == email && 
             accounts[i].password == passw) {
-                return User(accounts[i].user)
+                return searchUser(accounts[i].email)
             }
     }   
     return null
 }
 
+// Search for an user using email
+function searchUser(email) {
+    let users = getObj(FILE_USERS)
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email == email) {
+            return new User(users[i].name, users[i].email)
+        }}
+    throw Error('Email not found')
+}
+
 // Such a function will create an account and store it in FILE_ACCOUNT
+// and also create new user
 function signUp(email, passw) {
     let accounts = getObj(FILE_ACCOUNTS)    
     
     // If account does not exists
     if (accounts.indexOf(email) == -1) {
-        accounts.push({user: email, password: passw})
+        accounts.push({email: email, password: passw})
         
         const string = JSON.stringify(accounts)
         writeToFile(string, FILE_ACCOUNTS)
+        
+        createNewUser(email)
+
         return true
     }
     return null
+}
+
+// Will create a new user and stores it at users.json
+function createNewUser(email) {
+    let username = email.substring(0, email.indexOf('@'));
+    let user = {
+        name: username,
+        email: email
+    }
+    addToJSONFile(user, FILE_USERS)
+    return true
 }
 
 /* -=============== EXPRESS */
@@ -96,9 +130,22 @@ app.get('/', (req, res) => {
 // de registro (email e senha)
 // Chaves necessárias do request: email, passw
 app.post('/signup', (req, res) => {
-    signUp(req.body.email, req.body.passw)
-    res.send('post ' + req.query.email)
-    console.log(req.query.email)
+    let r = signUp(req.query.email, req.query.passw)
+    if (r == null) {
+        res.status(400).end("Wasn't possible to sign up")
+    } else {
+        res.end('Account created')
+    }
+})
+
+// Chaves necessárias do request: email, passw
+app.post('/login', (req, res) => {
+    let r = login(req.query.email, req.query.passw)
+    if (r == null) {
+        res.status(400).end("Wasn't possible to login")
+    } else {
+        res.end(JSON.stringify(r))
+    }
 })
 
 app.listen(port, () => {
